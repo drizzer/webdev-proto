d4897071c40ce28d203752f2947a380cccb9b4b1
drizzer <46766033+drizzer@users.noreply.github.com>
2023-10-01 23:39:33 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Heading, VStack } from "@chakra-ui/react";
+import * as React from "react";
+
+export function App() {
+  return (
+    <>
+      <Heading textAlign="center">Home Page</Heading>
+      <VStack></VStack>
+    </>
+  );
+}
---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+import "./styles.css";
+import { App } from "./App";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <React.StrictMode>
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
+);
